cfd35c4d912d371625fe879abb071b2e02096475
Kerminate <kepeilin@outlook.com>
2018-06-25 21:42:34 +0800

    feat: add music playback

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,3 +1,85 @@
+const dataList = require('../../../data/posts-data.js')
+
 Page({
-  
+  data: {
+    currentPostId: 0,
+    postData: [],
+    collected: '',
+    isPlayingMusic: false
+  },
+  onLoad: function (option) {
+    const postId = option.id
+    const postData = dataList.postList[postId]
+    this.setData({
+      currentPostId: postId,
+      postData: postData
+    })
+
+    const postsCollected = wx.getStorageSync('posts_collected')
+    if (postsCollected) {
+      const postCollected = postsCollected[postId]
+      this.setData({
+        collected: postCollected
+      })
+    } else {
+      const postsCollected = {}
+      postsCollected[postId] = false
+      wx.setStorageSync('posts_collected', postsCollected)
+    }
+  },
+  onCollectionTap: function () {
+    const postsCollected = wx.getStorageSync('posts_collected')
+    let postCollected = postsCollected[this.data.currentPostId]
+    postCollected = !postCollected
+    postsCollected[this.data.currentPostId] = postCollected
+    this.showToast(postsCollected, postCollected)
+  },
+  showToast: function (postsCollected, postCollected) {
+    wx.setStorageSync('posts_collected', postsCollected)
+    this.setData({
+      collected: postCollected
+    })
+    wx.showToast({
+      title: postCollected ? '收藏成功' : '取消成功',
+      duration: 1000
+    })
+  },
+  onShareTap: function () {
+    const itemList = [
+      '分享到微信好友',
+      '分享到朋友圈',
+      '分享到QQ',
+      '分享到微博'
+    ]
+    wx.showActionSheet({
+      itemList: itemList,
+      itemColor: '#405f80',
+      success: function (res) {
+        wx.showModal({
+          title: '用户 ' + itemList[res.tapIndex],
+          content: '现在还无法实现该功能，抱歉！'
+        })
+      }
+    })
+  },
+  onMusicTap: function () {
+    const currentPostId = this.data.currentPostId
+    const postData = dataList.postList[currentPostId]
+    const isPlayingMusic = this.data.isPlayingMusic
+    if (isPlayingMusic) {
+      wx.pauseBackgroundAudio()
+      this.setData({
+        isPlayingMusic: false
+      })
+    } else {
+      wx.playBackgroundAudio({
+        dataUrl: postData.music.url,
+        title: postData.music.title,
+        coverImgUrl: postData.music.coverImg
+      })
+      this.setData({
+        isPlayingMusic: true
+      })
+    }
+  }
 })
--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -12,7 +12,7 @@ Page({
   onPostTap: function (event) {
     const postId = event.currentTarget.dataset.postid
     wx.navigateTo({
-      url: 'post-detail/post-detail'
+      url: 'post-detail/post-detail?id=' + postId
     })
   }
 })
